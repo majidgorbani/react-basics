f3dc29c16453a8f0761eae7fec947dc2502ca06a
Majid Gorbani <Majid Gorbani>
2017-04-03 00:24:32 +0200

    dd

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Header } from './components/Header';
 
 class App extends React.Component{
     render(){
 
         return(
-            <div>
+            <div className="container">
+                <Header />
+                <div className="row">
+                     <div className="col-xs-10 col-xs-offset-1">
+                    
+                     <h1>hello</h1>
+                </div>
 
-                this is an app
+                </div>
+               
+
+               
                 
             </div>
 
