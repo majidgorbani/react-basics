412ae58b34a4665458fd03a2f7dc8535dd9cdc2b
Majid Gorbani <Majid Gorbani>
2017-04-02 22:54:29 +0200

    dsd

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,21 @@
-console.log("it w3orks");
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component{
+    render(){
+
+        return(
+            <div>
+
+                this is an app
+                
+            </div>
+
+        );
+    }
+
+
+}
+render(<App />,
+document.getElementById('app')
+)
