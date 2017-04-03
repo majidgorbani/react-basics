2c16bc43a5cfb6a731a69838f4f722da15cf5d7a
Majid Gorbani <majid.gorbani@acando.com>
2017-04-03 16:56:43 +0200

    gfg

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,25 @@
-console.log("it w3orks");
+
+import React from 'react';
+import {render} from 'react-dom';
+import {boostrap} from 'react-router-bootstrap'
+
+export class App extends React.Component{
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1> hello!</h1>
+                    </div>
+
+                </div>
+            </div>
+
+
+        
+        );
+    }
+}
+
+render(<App />, document.getElementById("app"));
